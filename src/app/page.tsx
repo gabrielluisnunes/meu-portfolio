68909294209746b716d53e5cc8170b65af217d88
gabrielluisnunes <gabrielluisnunes@gmail.com>
2025-11-10 23:58:44 -0300

    Footer Mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
             </SectionContainer>
             
             {/* {} */}
-            <Footer />
+            
             
         </MainContainer>
     );
